be148dc04a94841a912bf6da841e0638f9b1d6f3
Khushmeet <khushmeetsingh1996@gmail.com>
2016-12-01 23:28:19 +0530

    using XPlot to draw geo plots

--- country.fsx
+++ country.fsx
@@ -1,9 +1,9 @@
 #load "packages/FsLab/FsLab.fsx"
 
 open FSharp.Data
+open XPlot.GoogleCharts
 
 let wb = WorldBankData.GetDataContext()
-
 let capital = wb.Countries.India.CapitalCity
 let region = wb.Countries.India.Region
 let indicators = wb.Countries.India.Indicators.``CO2 emissions (kt)``
@@ -27,3 +27,27 @@ let tempForCity city =
 
 // Function call
 tempForCity "Chandigarh"
+
+
+// Temprature of all the countries
+let worldTemp =
+    [ for c in wb.Countries ->
+        let place = c.CapitalCity + "," + c.Name
+        printfn "Getting temperature in: %s" place 
+        c.Name, tempForCity place ]
+
+
+// Creates a Geo Map with countries and their temperature
+// (opens in a browser)
+Chart.Geo(worldTemp)
+
+// .Net array in F#
+let colors = [| "#80E000";"#E0C000";"#E07B00";"#E02800" |] 
+let values = [| -30;-10;+10;+30 |]
+let axis = ColorAxis(values=values, colors=colors)
+
+// Decorated Chart
+worldTemp
+|> Chart.Geo
+|> Chart.WithOptions(Options(colorAxis=axis)) 
+|> Chart.WithLabel "Temp"
